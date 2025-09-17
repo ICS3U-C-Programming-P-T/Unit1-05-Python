80f04e172c73d075429ad9e1e7372720bcb536c0
Patrick Student <patrick.tumbaga@stu.ocsb.ca>
2025-09-17 18:14:10 +0000

    “area_per_rect.py”

---
+++ area_per_rect.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Created By: Patrick
+# Date: 9/17/2025
+# My Area & Perimeter of Rectangle, in Python
+
+
+def main():
+    print("For the rectangle with a length of 6cm and a width of 7cm")
+    print("The area is {}cm^2".format(6 * 7))
+    print("The perimeter is: {}cm2^".format(2 * (6 + 7)))
+
+
+if __name__ == "__main__":
+    main()
